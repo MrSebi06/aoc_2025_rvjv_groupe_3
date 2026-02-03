960565f3e79cb8cc49a8ac9866034ad09170dd28
MrSebi06 <mihneasebastian06@gmail.com>
2026-02-03 17:15:43 +0100

    Add d1 example

---
+++ src/d1/mod.rs
@@ -0,0 +1,18 @@
+pub fn p1(input: &str) -> usize {
+    todo!()
+}
+
+pub fn p2(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::d1::p1;
+
+    #[test]
+    fn p1_test() {
+        let input = include_str!("d1_test.txt");
+        assert_eq!(p1(input), 3)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod d1;
+
 fn main() {
-    println!("Hello, world!");
+    println!("d1p1: {}", d1::p1(include_str!("d1/d1.txt")));
+    println!("d1p2: {}", d1::p2(include_str!("d1/d1.txt")));
 }
