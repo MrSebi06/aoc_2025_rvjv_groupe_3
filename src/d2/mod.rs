be8f994145ed4f0937f040c399d6f0e7675c7c74
MrSebi06 <mihneasebastian06@gmail.com>
2026-02-05 18:06:16 +0100

    D2P1 V1

---
+++ src/d2/mod.rs
@@ -0,0 +1,59 @@
+
+fn is_invalid_id_p1(id: String) -> bool
+{
+    let len = id.len();
+    if len % 2 != 0
+    {
+        return false;
+    }
+
+    let (first, last) = id.split_at(len/2);
+
+    first == last
+}
+
+pub fn p1(input: &str) -> u64 {
+    let segments = input.split(",");
+    let mut total = 0;
+
+    for segment in segments
+    {
+        let mut bounds = segment.split('-');
+        let lower: u64 = bounds.next().expect("lower not found").to_string().parse().expect("lower is not u32");
+        let upper: u64 = bounds.next().expect("upper not found").to_string().parse().expect("upper is not u32");
+
+        for i in lower..=upper
+        {
+            let i_str = i.to_string();
+
+            if is_invalid_id_p1(i_str)
+            {
+                total += i;
+            }
+        }
+    }
+
+    total
+}
+
+pub fn p2(input: &str) -> usize {
+    0
+
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::d2::{p1, p2};
+
+    #[test]
+    fn p1_test() {
+        let input = include_str!("d2_test.txt");
+        assert_eq!(p1(input), 1227775554)
+    }
+
+    #[test]
+    fn p2_test() {
+        let input = include_str!("d2_test.txt");
+        assert_eq!(p2(input), 6)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod d1;
+mod d2;
 
 fn main() {
-    println!("d1p1: {}", d1::p1(include_str!("d1/d1.txt")));
-    println!("d1p2: {}", d1::p2(include_str!("d1/d1.txt")));
+    // D1
+    // println!("d1p1: {}", d1::p1(include_str!("d1/d1.txt")));
+    // println!("d1p2: {}", d1::p2(include_str!("d1/d1.txt")));
+
+    // D2
+    println!("d1p1: {}", d2::p1(include_str!("d2/d2.txt")));
+    println!("d1p2: {}", d2::p2(include_str!("d2/d2.txt")));
 }
