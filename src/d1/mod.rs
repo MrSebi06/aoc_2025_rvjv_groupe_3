08e051aa1787363a2c54f2afbefe44e2f61d6b76
MrSebi06 <mihneasebastian06@gmail.com>
2026-02-05 17:16:00 +0100

    D1P1 V1

--- src/d1/mod.rs
+++ src/d1/mod.rs
@@ -1,9 +1,29 @@
+use std::ops::Rem;
+
 pub fn p1(input: &str) -> usize {
-    todo!()
+    let mut cur_pos = 50;
+    let mut counter = 0;
+
+    for line in input.lines() {
+        let mut line_str = line.to_string();
+        let direction = line_str.remove(0);
+        let distance: i16 = line_str.parse().expect("NaN");
+
+        let direction: i16 = match direction {
+            'L' => -1,
+            _ => 1
+        };
+
+        cur_pos = (cur_pos + (direction * distance)) % 100;
+        if cur_pos == 0 {
+            counter += 1
+        }
+    }
+    counter
 }
 
 pub fn p2(input: &str) -> usize {
-    todo!()
+    1
 }
 
 #[cfg(test)]
