8ea628f5943b96790bf6eaa264ef9c70a6ef1ef6
MrSebi06 <mihneasebastian06@gmail.com>
2026-02-05 17:42:20 +0100

    D1P2 V1

--- src/d1/mod.rs
+++ src/d1/mod.rs
@@ -1,5 +1,3 @@
-use std::ops::Rem;
-
 pub fn p1(input: &str) -> usize {
     let mut cur_pos = 50;
     let mut counter = 0;
@@ -23,16 +21,45 @@ pub fn p1(input: &str) -> usize {
 }
 
 pub fn p2(input: &str) -> usize {
-    1
+    let mut cur_pos = 50;
+    let mut counter = 0;
+
+    for line in input.lines() {
+        let mut line_str = line.to_string();
+        let direction = line_str.remove(0);
+
+        let distance: i16 = line_str.parse().expect("NaN");
+        counter += distance as usize / 100;
+        let distance = distance % 100;
+
+        let direction: i16 = match direction {
+            'L' => -1,
+            _ => 1
+        };
+
+        if (direction == -1 && distance >= cur_pos && cur_pos != 0) {
+            counter += 1;
+        } else if (direction == 1 && distance >= 100 - cur_pos  && cur_pos != 0) {
+            counter += 1;
+        }
+        cur_pos = (cur_pos + (direction * distance)).rem_euclid(100);
+    }
+    counter
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::d1::p1;
+    use crate::d1::{p1, p2};
 
     #[test]
     fn p1_test() {
         let input = include_str!("d1_test.txt");
         assert_eq!(p1(input), 3)
+    }
+
+    #[test]
+    fn p2_test() {
+        let input = include_str!("d1_test.txt");
+        assert_eq!(p2(input), 6)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod d1;
 
 fn main() {
-    // println!("d1p1: {}", d1::p1(include_str!("d1/d1_test.txt")));
-    // println!("d1p2: {}", d1::p2(include_str!("d1/d1_test.txt")));
     println!("d1p1: {}", d1::p1(include_str!("d1/d1.txt")));
     println!("d1p2: {}", d1::p2(include_str!("d1/d1.txt")));
 }
