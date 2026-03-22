d5d763ffa28c79e36fcdf94c08313e45f8e332be
MrSebi06 <mihneasebastian06@gmail.com>
2026-03-22 15:52:10 +0100

    Optim d10p1
    Co-authored-by: Michaël ATTAL <michaelattal@icloud.com>
    Co-authored-by: Sacha TOUTUT <sacha@toutut.fr>

--- src/d10/mod.rs
+++ src/d10/mod.rs
@@ -1,57 +1,57 @@
 use std::collections::VecDeque;
 
 #[derive(Default)]
-struct BfsBuffer
-{
+struct BfsBuffer {
     lights_state: Vec<bool>,
     button_index: usize,
-    depth: u64
+    remaining_buttons: Vec<usize>,
+    depth: u64,
 }
 
-pub fn check_res(expected: &Vec<bool>, current: &Vec<bool>) -> bool
-{
-    for i in 0..expected.len()
-    {
-        if expected[i] != current[i] { return false; }
+pub fn check_res(expected: &Vec<bool>, current: &Vec<bool>) -> bool {
+    for i in 0..expected.len() {
+        if expected[i] != current[i] {
+            return false;
+        }
     }
 
     true
 }
 
 pub fn p1(input: &str) -> u64 {
-
     let mut count = 0;
 
-    for line in input.lines()
-    {
+    for line in input.lines() {
         // Input format is messy, long process to treat it correctly
         let mut split = line.split(" ");
 
         // First is always the expected lights display, so we convert it to a vec of booleans
         let expected_lights_str = split.next().unwrap();
-        let expected_lights_str = &expected_lights_str[1..expected_lights_str.len()-1];
+        let expected_lights_str = &expected_lights_str[1..expected_lights_str.len() - 1];
 
         let mut expected_lights = vec![false; 0];
-        for c in expected_lights_str.chars()
-        {
+        for c in expected_lights_str.chars() {
             match c {
-                '.' => { expected_lights.push(false) }
-                '#' => { expected_lights.push(true) }
+                '.' => expected_lights.push(false),
+                '#' => expected_lights.push(true),
                 _ => {}
             }
         }
 
         let mut buttons = vec![vec![0; 0]; 0];
-        for (i, button_str) in split.enumerate()
-        {
+        for (i, button_str) in split.enumerate() {
             // Part 1, we can skip voltages
-            if button_str.starts_with('{') { continue }
+            if button_str.starts_with('{') {
+                continue;
+            }
 
             let mut triggers = vec![0; 0];
 
-            let button_str = &button_str[1..button_str.len()-1];
+            let button_str = &button_str[1..button_str.len() - 1];
             let button_triggers = button_str.split(",");
-            for trig_str in button_triggers { triggers.push(trig_str.parse::<usize>().unwrap()) }
+            for trig_str in button_triggers {
+                triggers.push(trig_str.parse::<usize>().unwrap())
+            }
 
             buttons.push(triggers);
         }
@@ -61,23 +61,43 @@ pub fn p1(input: &str) -> u64 {
         // There's probably more optimal but I'm short on sanity and time so we'll just...
 
         let mut queue: VecDeque<BfsBuffer> = VecDeque::new();
-        for i in 0..buttons.len()
-        {
+        let rem_buttons_global: Vec<usize> = (0..buttons.len()).collect();
+        for i in 0..buttons.len() {
+            let mut rem_buttons = rem_buttons_global.clone();
+            rem_buttons.remove(i);
+
             let buffer = BfsBuffer {
                 lights_state: vec![false; expected_lights.len()],
                 button_index: i,
-                depth: 1
+                remaining_buttons: rem_buttons,
+                depth: 1,
             };
             queue.push_back(buffer)
         }
 
+        let mut explored_states: Vec<Vec<bool>> = vec![vec![false; expected_lights.len()]; 1];
         while !queue.is_empty() {
             let buffer = queue.pop_front().unwrap();
             let button = &buttons[buffer.button_index];
 
             let mut lights_state = buffer.lights_state;
             // Apply button operations to our current state
-            for i in button { lights_state[*i] = !lights_state[*i] }
+            for i in button {
+                lights_state[*i] = !lights_state[*i]
+            }
+
+            let mut explored = false;
+            for explored_state in explored_states.iter() {
+                if check_res(&lights_state, &explored_state) {
+                    explored = true;
+                    break;
+                }
+            }
+            if explored {
+                continue;
+            }
+
+            explored_states.push(lights_state.clone());
 
             // Test if current result fits our expected result...
             if check_res(&expected_lights, &lights_state) {
@@ -88,12 +108,15 @@ pub fn p1(input: &str) -> u64 {
             // If not, then start pushing further possibilities into the queue
             // TODO: Trim the sent possibilities so we don't lose time on useless ones (For example using the same button twice)
             // That is easily the best way to optimize this approach
-            for i in 0..buttons.len()
-            {
+            for i in buffer.remaining_buttons.iter() {
+                let mut rem_buttons = buffer.remaining_buttons.clone();
+                rem_buttons.remove(rem_buttons.iter().position(|&r| &r == i).unwrap());
+
                 let new_buffer = BfsBuffer {
                     lights_state: lights_state.clone(),
-                    button_index: i,
-                    depth: buffer.depth + 1
+                    button_index: *i,
+                    remaining_buttons: rem_buttons,
+                    depth: buffer.depth + 1,
                 };
                 queue.push_back(new_buffer)
             }
